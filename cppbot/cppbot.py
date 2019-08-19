bf7b786dc4c77384ea4a800117636987e93a836a
Darren Kam <darrenmkam4150@gmail.com>
2019-08-19 01:19:18 -1000

    Reorganize code and logging

--- cppbot/cppbot.py
+++ cppbot/cppbot.py
@@ -1,12 +1,11 @@
-"""This module contains the bot code for the Cpp Beginners Discord Bot."""
+"""This module contains the bot code for the C++ Beginners Discord Bot."""
 
 import argparse
 import logging
 import os
-import sys
 
 from discord.ext import commands
-from leaderboard import leaderboard
+from leaderboard.leaderboard import Leaderboard
 
 from cppbot import utils
 from cppbot.constants import DEFAULT_CONFIG_PATH, DEFAULT_CONFIG_SETTINGS
@@ -17,22 +16,31 @@ from cppbot.utils import logger
 class CppBot(commands.Bot):
     """Custom Discord client for the CppBot."""
     def __init__(self, redis_password):
+        # Prefix commands with character
         super().__init__('!')
 
-        try:
-            keys = utils.get_redis_keys(redis_password)
-        except Exception as error:
-            sys.exit(error)
-
         self.redis_password = redis_password
-        self.lboards = {
-            lboard: leaderboard.Leaderboard(lboard, page_size=10, password=redis_password)
-            for lboard in keys
-        }
+
+        # Initialize leaderboards
+        self.lboards = {}
+        for lboard in utils.get_redis_keys(redis_password):
+            self.lboards[lboard] = Leaderboard(lboard, page_size=10, password=redis_password)
 
     async def on_ready(self):
         """Logged on event."""
-        logging.info('Logged on as %s', self.user)
+        logger.info('Logged on as %s', self.user)
+
+
+def load_config(config_path):
+    """Loads a config file."""
+    try:
+        config = utils.read_config(config_path)
+    except IOError:
+        logger.warning('No config found... generating default config')
+        config = utils.generate_config(config_path, DEFAULT_CONFIG_SETTINGS)
+
+    logger.info('Loaded config file')
+    return config
 
 
 def _main():
@@ -44,31 +52,19 @@ def _main():
     parser.add_argument('--config', default=DEFAULT_CONFIG_PATH, help='specify a config file')
     args = parser.parse_args()
 
-    # Configuration file
-    config_path = args.config
-    try:
-        config = utils.read_config(config_path)
-    except IOError:
-        logger.warning('No config found... generating default config')
-        config = utils.generate_config(config_path, DEFAULT_CONFIG_SETTINGS)
-    logger.debug('Loaded config file')
+    config = load_config(args.config)
 
     log_file = config['Debug Settings']['LOG_FILE']
     log_level = config['Debug Settings']['LOGGING_LEVEL']
 
     # Set logging settings
     utils.configure_logger(log_level, logger, log_file=log_file)
+    # Only show warnings from the discord logger
     utils.configure_logger(logging.WARNING, logging.getLogger('discord'))
 
     # Get secret keys from environment
-    try:
-        discord_key = os.environ['DISCORD_KEY']
-    except KeyError:
-        sys.exit('Cannot find discord key')
-    try:
-        redis_key = os.environ['REDIS_KEY']
-    except KeyError:
-        sys.exit('Cannot find redis key')
+    discord_key = os.environ['DISCORD_KEY']
+    redis_key = os.environ['REDIS_KEY']
 
     # Start the discord client
     client = CppBot(redis_key)
@@ -83,8 +79,4 @@ def _main():
 
 
 if __name__ == '__main__':
-    try:
-        _main()
-    except SystemExit as e:
-        logger.critical(e)
-        sys.exit(1)
+    _main()
