97f323fae365c5170b7eaca82782433da4aff73b
Darren Kam <darrenmkam4150@gmail.com>
2019-08-21 00:36:55 -1000

    Refactor leaderboard extension

--- cppbot/extensions/leaderboard.py
+++ cppbot/extensions/leaderboard.py
@@ -1,4 +1,4 @@
-"""Leaderboard extension."""
+"""The Leaderboard extension."""
 
 import logging
 
@@ -6,170 +6,253 @@ from discord.ext import commands
 from leaderboard.leaderboard import Leaderboard
 from texttable import Texttable
 
+logger = logging.getLogger(__name__)
+
 
 def setup(bot):
-    """Set up extension."""
+    """Add the Leaderboard cog to the Discord bot."""
     bot.add_cog(LeaderboardCog(bot))
+
+    logger.info('Added Leaderboard cog')
 
 
 def leaderboard_list(lboard, page):
-    """Create list from leaderboard."""
-    lboard_list = [['rank', 'member', 'score']]
+    """Get a leaderboard page as a list.
+
+    Args:
+        lboard (leaderboard.leaderboard.Leaderboard): The leaderboard.
+        page (int): The leaderboard page.
+
+    Returns:
+        list: A 2D list representation of the leaderboard.
+    """
+    lboard_list = [
+        ['rank', 'member', 'score']
+    ]
+
+    # Append rows to leaderboard list
     for row in lboard.leaders(page):
         lboard_list.append([row['rank'], row['member'].decode('utf-8'), row['score']])
+
     return lboard_list
 
 
-def _get_lboard_table(lboard, page):
-    """Get the leaderboard as a text table."""
+def get_lboard_text(lboard, page):
+    """Get a leaderboard page as a text table.
+
+    Args:
+        lboard (leaderboard.leaderboard.Leaderboard): The leaderboard.
+        page (int): The leaderboard page.
+
+    Returns:
+        str: The leaderboard as a text table.
+    """
     table = Texttable()
+
+    # Set text alignment
     table.set_cols_align(['c', 'c', 'c'])
     table.set_cols_valign(['m', 'm', 'm'])
 
+    # Import the leaderboard into the table
     table.add_rows(leaderboard_list(lboard, page))
 
     return table.draw()
 
 
 class LeaderboardCog(commands.Cog, name='Leaderboard'):
-    """Commands for the leaderboard leaderboards."""
+    """Commands for the leaderboards."""
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command('addlb')
-    async def add_leaderboard(self, ctx, leaderboard, points=100):
-        """Add a new leaderboard."""
-        points = float(points)
+    @commands.group()
+    async def leaderboard(self, ctx):
+        """The leaderboard command group."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Invalid leaderboard command passed...')
 
+    @leaderboard.group()
+    async def user(self, ctx):
+        """The leaderboard user subcommand."""
+        if ctx.invoked_subcommand is self.user:
+            await ctx.send('Invalid user command passed...')
+
+    # TODO - Implement point limiting
+    @leaderboard.command('add')
+    async def add_leaderboard(self, ctx, leaderboard, points=100):
+        """Add a leaderboard.
+
+        Args:
+            leaderboard (str): The leaderboard name.
+            points (int, optional): The max number of points available. Defaults to 100.
+        """
         if leaderboard in self.bot.lboards.keys():
             await ctx.send('Leaderboard already exists!')
             return
 
-        self.bot.lboards[leaderboard] = Leaderboard(leaderboard, page_size=10,
-                                                    password=self.bot.redis_key)
+        self.bot.lboards[leaderboard] = Leaderboard(leaderboard, page_size=10, password=self.bot.redis_key)
 
-        logging.info('Added the leaderboard "%s" with %f points', leaderboard, points)
+        logger.info('Added the leaderboard "%s" with %f points', leaderboard, points)
 
-    @commands.command('dellb')
-    async def del_leaderboard(self, ctx, leaderboard):
-        """Removes a leaderboard."""
+    @leaderboard.command('remove')
+    async def remove_leaderboard(self, ctx, leaderboard):
+        """Remove a leaderboard.
+
+        Args:
+            leaderboard (str): The leaderboard name.
+        """
         if leaderboard not in self.bot.lboards.keys():
-            await ctx.send('Leaderboard doesn\'t exist!')
+            await ctx.send('Leaderboard does not exist!')
             return
 
         self.bot.lboards[leaderboard].delete_leaderboard()
         self.bot.lboards.pop(leaderboard)
 
-        logging.info('Removed the leaderboard "%s"', leaderboard)
+        logger.info('Removed the leaderboard "%s"', leaderboard)
 
-    @commands.command('addpoints')
-    async def add_points(self, ctx, user, leaderboard, points):
-        """Add points to a user's leaderboard score."""
-        points = float(points)
+    @leaderboard.command('show')
+    async def show_leaderboard(self, ctx, leaderboard, page=1):
+        """Show the specified leaderboard.
 
+        Args:
+            leaderboard (str): The leaderboard name.
+            page (int, optional): The leaderboard page to display. Defaults to 1.
+        """
         if leaderboard not in self.bot.lboards.keys():
-            await ctx.send('Leaderboard doesn\'t exist!')
+            await ctx.send('Leaderboard does not exist!')
             return
-        if points < 0:
+        elif not self.bot.lboards[leaderboard].total_members():
+            await ctx.send('Cannot display empty leaderboard!')
+            return
+        elif page < 1 or self.bot.lboards[leaderboard].total_pages() < page:
+            await ctx.send('Invalid page number')
+            return
+
+        leaderboard_msg = '```' \
+                          + 'Leaderboard "' + leaderboard + '"\n\n' + \
+                          get_lboard_text(self.bot.lboards[leaderboard], page) \
+                          + '\n\n**Page ' + str(page) + '**' \
+                          + '```'
+        await ctx.send(leaderboard_msg)
+
+        logger.info('Displayed leaderboard "%s" page %d', leaderboard, page)
+
+    @leaderboard.command('list')
+    async def list_leaderboards(self, ctx):
+        """List all leaderboards."""
+        lboard_list = '\n'.join(self.bot.lboards.keys())
+
+        await ctx.send('```\n{0}\n```'.format(lboard_list))
+
+    @user.command('add')
+    async def add_user(self, ctx, user, leaderboard):
+        """Add a user to a leaderboard.
+
+        Args:
+            user (str): A Discord username.
+            leaderboard (str): The leaderboard name.
+        """
+        if leaderboard not in self.bot.lboards.keys():
+            await ctx.send('Leaderboard does not exist!')
+            return
+        elif self.bot.lboards[leaderboard].check_member(user):
+            await ctx.send('User is already on the leaderboard!')
+            return
+
+        self.bot.lboards[leaderboard].rank_member(user, 0)
+
+        logger.info('Added the user "%s" to leaderboard "%s"', user, leaderboard)
+
+    @user.command('remove')
+    async def remove_user(self, ctx, user, leaderboard):
+        """Remove a user from a leaderboard.
+
+        Args:
+            user (str): A Discord username.
+            leaderboard (str): The leaderboard name.
+        """
+        if leaderboard not in self.bot.lboards.keys():
+            await ctx.send('Leaderboard does not exist!')
+            return
+        elif not self.bot.lboards[leaderboard].check_member(user):
+            await ctx.send('User is not on the leaderboard!')
+            return
+
+        self.bot.lboards[leaderboard].remove_member(user)
+
+        logger.info('Removed the user "%s" from leaderboard "%s"', user, leaderboard)
+
+    # TODO - Test what if user points is not set
+    @user.command('give')
+    async def give_points(self, ctx, user, leaderboard, points):
+        """Give points to a user.
+
+        Args:
+            user (str): A Discord username.
+            leaderboard (str): The leaderboard name.
+            points (int or float): The number of points to give.
+        """
+        if leaderboard not in self.bot.lboards.keys():
+            await ctx.send('Leaderboard does not exist!')
+            return
+        elif not self.bot.lboards[leaderboard].check_member(user):
+            await ctx.send('User is not on the leaderboard!')
+            return
+        elif points < 0:
             await ctx.send('Points should be greater than 0!')
             return
 
         current_points = self.bot.lboards[leaderboard].score_for(user)
         if current_points:
             points += current_points
+
         self.bot.lboards[leaderboard].rank_member(user, points)
 
-        logging.info('Gave the user "%s" %f points', user, points)
+        logger.info('Gave the user "%s" %f points', user, points)
 
-    @commands.command('delpoints')
-    async def del_points(self, ctx, user, leaderboard, points):
-        """Remove points from a user's leaderboard score."""
-        points = float(points)
+    # TODO - Test what if user points is not set
+    @user.command('take')
+    async def take_points(self, ctx, user, leaderboard, points):
+        """Take points from a user.
 
+        Args:
+            user (str): A Discord username.
+            leaderboard (str): The leaderboard name.
+            points (int or float): The number of points to take.
+        """
         if leaderboard not in self.bot.lboards.keys():
-            await ctx.send('Leaderboard doesn\'t exist!')
+            await ctx.send('Leaderboard does not exist!')
             return
-        if not self.bot.lboards[leaderboard].check_member(user):
-            await ctx.send('User doesn\'t exist in the leaderboard!')
+        elif not self.bot.lboards[leaderboard].check_member(user):
+            await ctx.send('User is not on the leaderboard!')
             return
-        if points < 0:
+        elif points < 0:
             await ctx.send('Points should be greater than 0!')
             return
 
         current_points = self.bot.lboards[leaderboard].score_for(user)
         if current_points:
             points -= current_points
+
         self.bot.lboards[leaderboard].rank_member(user, points)
 
-        logging.info('Docked the user "%s" -%f points', user, points)
+        logger.info('Docked the user "%s" -%f points', user, points)
 
-    @commands.command('setpoints')
+    @user.command('set')
     async def set_points(self, ctx, user, leaderboard, points):
-        """Set a user's leaderboard score."""
-        points = float(points)
+        """Set a user's points.
 
+        Args:
+            user (str): A Discord username.
+            leaderboard (str): The leaderboard name.
+            points (int or float): The user's new number of points.
+        """
         if leaderboard not in self.bot.lboards.keys():
-            await ctx.send('Leaderboard doesn\'t exist!')
-            return
-
-        self.bot.lboards[leaderboard].rank_member(user, points)
-
-        logging.info('Set the user "%s" to %f points', user, points)
-
-    @commands.command('addentry')
-    async def add_entry(self, ctx, user, leaderboard):
-        """Add a user entry to a leaderboard."""
-        if leaderboard not in self.bot.lboards.keys():
-            await ctx.send('Leaderboard doesn\'t exist!')
-            return
-        if self.bot.lboards[leaderboard].check_member(user):
-            await ctx.send('User already exists in the leaderboard!')
-            return
-
-        self.bot.lboards[leaderboard].rank_member(user, 0)
-
-        logging.info('Added the user entry "%s" to leaderboard %s', user, leaderboard)
-
-    @commands.command('delentry')
-    async def del_entry(self, ctx, user, leaderboard):
-        """Removes a user entry from a leaderboard."""
-        if leaderboard not in self.bot.lboards.keys():
-            await ctx.send('Leaderboard doesn\'t exist!')
-            return
-        if not self.bot.lboards[leaderboard].check_member(user):
-            await ctx.send('User doesn\'t exist in the leaderboard!')
-            return
-
-        self.bot.lboards[leaderboard].remove_member(user)
-
-        logging.info('Removed the user entry "%s" from leaderboard %s', user, leaderboard)
-
-    @commands.command('lb')
-    async def show_leaderboard(self, ctx, lboard_name, lboard_page=1):
-        """Display the specified leaderboard."""
-        if lboard_name not in self.bot.lboards.keys():
             await ctx.send('Leaderboard does not exist!')
             return
-        if not self.bot.lboards[lboard_name].total_members():
-            await ctx.send('Cannot display empty leaderboard!')
-            return
-        if lboard_page < 1 or self.bot.lboards[lboard_name].total_pages() < lboard_page:
-            await ctx.send('Invalid page number')
+        elif not self.bot.lboards[leaderboard].check_member(user):
+            await ctx.send('User is not on the leaderboard!')
             return
 
-        leaderboard_msg = '```' \
-                          + 'Leaderboard "' + lboard_name + '"\n\n' + \
-                          _get_lboard_table(self.bot.lboards[lboard_name],
-                                            lboard_page) \
-                          + '\n\n**Page ' + str(lboard_page) + '**' \
-                          + '```'
-        await ctx.send(leaderboard_msg)
+        self.bot.lboards[leaderboard].rank_member(user, points)
 
-        logging.info('Displayed leaderboard "%s" page %d', lboard_name, lboard_page)
-
-    @commands.command('listlbs')
-    async def list_leaderboards(self, ctx):
-        """List all leaderboard names."""
-        lboard_list = '\n'.join(self.bot.lboards.keys())
-
-        await ctx.send('```\n{0}\n```'.format(lboard_list))
+        logger.info('Set the user "%s" to %f points', user, points)
